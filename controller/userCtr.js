ffd99a7ad9348ec7cde4c82c143700e75a015b94
Rahul vinod <120365941+raahulvinod@users.noreply.github.com>
2023-06-03 11:58:08 +0530

    Get All Users

--- controller/userCtr.js
+++ controller/userCtr.js
@@ -36,4 +36,15 @@ const loginUserCtrl = asyncHandler(async (req, res) => {
   }
 });
 
-module.exports = { createUser, loginUserCtrl };
+// Get all users
+
+const getAllUser = asyncHandler(async (req, res) => {
+  try {
+    const getUsers = await User.find();
+    res.json(getUsers);
+  } catch (error) {
+    throw new Error(error);
+  }
+});
+
+module.exports = { createUser, loginUserCtrl, getAllUser };
--- routes/authRoutes.js
+++ routes/authRoutes.js
@@ -1,8 +1,13 @@
 const express = require('express');
-const { createUser, loginUserCtrl } = require('../controller/userCtr');
+const {
+  createUser,
+  loginUserCtrl,
+  getAllUser,
+} = require('../controller/userCtr');
 const router = express.Router();
 
 router.post('/register', createUser);
 router.post('/login', loginUserCtrl);
+router.get('/all-users', getAllUser);
 
 module.exports = router;
