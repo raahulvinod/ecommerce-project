7f9287dc22ed36428a312dd55eaf77049e5a5386
Rahul vinod <120365941+raahulvinod@users.noreply.github.com>
2023-06-03 10:48:42 +0530

    Error Handling

--- controller/userCtr.js
+++ controller/userCtr.js
@@ -1,6 +1,7 @@
 const User = require('../models/userModel');
+const asyncHandler = require('express-async-handler');
 
-const createUser = async (req, res) => {
+const createUser = asyncHandler(async (req, res) => {
   const email = req.body.email;
   const findUser = await User.findOne({ email });
 
@@ -10,11 +11,8 @@ const createUser = async (req, res) => {
     res.json(newUser);
   } else {
     // User already exists
-    res.json({
-      message: 'User Already Exists',
-      success: false,
-    });
+    throw new Error('User Already Exists');
   }
-};
+});
 
 module.exports = { createUser };
--- index.js
+++ index.js
@@ -6,12 +6,16 @@ const bodyParser = require('body-parser');
 const PORT = process.env.PORT || 4000;
 
 const authRoutes = require('./routes/authRoutes');
+const { notFound, errorHandler } = require('./middlewares/errorHandler');
 
 dbConnect();
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 
 app.use('/api/user', authRoutes);
+
+app.use(notFound);
+app.use(errorHandler);
 
 app.listen(PORT, () => {
   console.log(`Server is running at PORT ${PORT}`);
