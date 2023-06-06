ed07fcc5dee611f7c301f02d1480a185f947e4e5
Rahul vinod <120365941+raahulvinod@users.noreply.github.com>
2023-06-06 23:32:06 +0530

    Pagination Query

--- controller/productCtrl.js
+++ controller/productCtrl.js
@@ -91,6 +91,18 @@ const getAllProduct = asyncHandler(async (req, res) => {
       query = query.select('-__v');
     }
 
+    // Pagination
+
+    const page = req.query.page;
+    const limit = req.query.limit;
+    const skip = (page - 1) * limit;
+    query = query.skip(skip).limit(limit);
+    if (req.query.page) {
+      const productCount = await Product.countDocuments();
+      if (skip >= productCount) throw new Error('This Page does not exists');
+    }
+    console.log(page, limit, skip);
+
     const product = await query;
     res.json(product);
   } catch (error) {
