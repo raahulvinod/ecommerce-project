36168a7e2d122c07f312296aab389273c7a2cc5a
Rahul vinod <120365941+raahulvinod@users.noreply.github.com>
2023-06-06 16:05:18 +0530

    Product Schema Modified

--- models/productModel.js
+++ models/productModel.js
@@ -22,12 +22,12 @@ var productSchema = new mongoose.Schema(
       required: true,
     },
     category: {
-      type: mongoose.Schema.Types.ObjectId,
-      ref: 'Category',
+      type: String,
+      required: true,
     },
     brand: {
       type: String,
-      enum: ['Apple', 'Samsung', 'Lenovo'],
+      required: true,
     },
     quantity: {
       type: Number,
@@ -42,7 +42,7 @@ var productSchema = new mongoose.Schema(
     },
     color: {
       type: String,
-      enum: ['Black', 'Brown', 'Red'],
+      required: true,
     },
     ratings: [
       {
