d547c6ffa31a1540f9359fb915103bbc8d5b5d6a
Rahul vinod <120365941+raahulvinod@users.noreply.github.com>
2023-06-09 11:13:36 +0530

    Product Review Commenting functioanlity added

--- controller/productCtrl.js
+++ controller/productCtrl.js
@@ -143,7 +143,7 @@ const addToWishList = asyncHandler(async (req, res) => {
 
 const rating = asyncHandler(async (req, res) => {
   const { _id } = req.user;
-  const { star, prodId } = req.body;
+  const { star, prodId, comment } = req.body;
   try {
     const product = await Product.findById(prodId);
     let alreadyRated = product.ratings.find(
@@ -155,7 +155,7 @@ const rating = asyncHandler(async (req, res) => {
           ratings: { $elemMatch: alreadyRated },
         },
         {
-          $set: { 'ratings.$.star': star },
+          $set: { 'ratings.$.star': star, 'ratings.$.comment': comment },
         },
         {
           new: true,
@@ -168,6 +168,7 @@ const rating = asyncHandler(async (req, res) => {
           $push: {
             ratings: {
               star: star,
+              comment: comment,
               postedby: _id,
             },
           },
--- models/productModel.js
+++ models/productModel.js
@@ -48,6 +48,7 @@ var productSchema = new mongoose.Schema(
     ratings: [
       {
         star: Number,
+        comment: String,
         postedby: { type: mongoose.Schema.Types.ObjectId, ref: 'User' },
       },
     ],
