8db61c8949aafaded58830db538a61bdbeb5a140
Rahul vinod <120365941+raahulvinod@users.noreply.github.com>
2023-06-09 12:37:37 +0530

    Cloudinary Configs created

---
+++ utils/cloudinary.js
@@ -0,0 +1,23 @@
+const cloudinary = require('cloudinary');
+cloudinary.config({
+  cloud_name: process.env.CLOUDINARY_CLOUD_NAME,
+  api_key: CLOUDINARY_API_KEY,
+  api_secret: CLOUDINARY_SECRET_KEY,
+});
+
+const cloudinaryUploadImg = async (fileToUploads) => {
+  return new Promise((resolve) => {
+    cloudinary.uploader.upload(fileToUploads, (result) => {
+      resolve(
+        {
+          url: result.secure_url,
+        },
+        {
+          resource_type: 'auto',
+        }
+      );
+    });
+  });
+};
+
+module.exports = cloudinaryUploadImg;
