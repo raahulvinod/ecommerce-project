08366b908dc81d21e651d62cf35572b724ba4a97
Rahul vinod <120365941+raahulvinod@users.noreply.github.com>
2023-06-11 13:29:06 +0530

    EnqSchema Created

---
+++ models/enqModel.js
@@ -0,0 +1,22 @@
+const mongoose = require('mongoose');
+
+var enqSchema = new mongoose.Schema({
+  name: {
+    type: String,
+    required: true,
+  },
+  email: {
+    type: String,
+    required: true,
+  },
+  mobile: {
+    type: String,
+    required: true,
+  },
+  comment: {
+    type: String,
+    required: true,
+  },
+});
+
+module.exports = mongoose.model('Enquiry', enqSchema);
