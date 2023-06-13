8466e755b32c7fcaba7139451a205966e0f4f180
Rahul vinod <120365941+raahulvinod@users.noreply.github.com>
2023-06-13 14:18:01 +0530

    Services section Added

--- frontend/src/pages/Home.js
+++ frontend/src/pages/Home.js
@@ -7,8 +7,8 @@ const Home = () => {
       <section className="home-wrapper-1 py-5">
         <div className="container-xxl">
           <div className="row">
-            <div className="col-6 ">
-              <div className="main-banner position-relative p-3">
+            <div className="col-6">
+              <div className="main-banner position-relative">
                 <img
                   className="img-fluid rounded-3"
                   src="images/main-banner-1.jpg"
@@ -17,14 +17,14 @@ const Home = () => {
                 <div className="main-banner-content position-absolute">
                   <h4>SUPERCHARGED FOR PROS.</h4>
                   <h5>iPad S13+ Pro.</h5>
-                  <p>From $999.00 or $41.62/mo. Footnote</p>
+                  <p>From $999.00 or $41.62/mo. </p>
                   <Link className="button">BUY NOW</Link>
                 </div>
               </div>
             </div>
             <div className="col-6">
-              <div className="d-flex flex-wrap gap-10 justify-content-between aligin-items-center">
-                <div className="small-banner position-relative p-3">
+              <div className="d-flex flex-wrap gap-10 justify-content-between align-items-center">
+                <div className="small-banner position-relative">
                   <img
                     className="img-fluid rounded-3"
                     src="images/catbanner-01.jpg"
@@ -33,11 +33,11 @@ const Home = () => {
                   <div className="small-banner-content position-absolute">
                     <h4>best sale</h4>
                     <h5>Laptops</h5>
-                    <p>From $999.00 or $41.62/mo.</p>
+                    <p>From $999.00.</p>
                   </div>
                 </div>
 
-                <div className="small-banner position-relative p-3">
+                <div className="small-banner position-relative">
                   <img
                     className="img-fluid rounded-3"
                     src="images/catbanner-02.jpg"
@@ -46,11 +46,11 @@ const Home = () => {
                   <div className="small-banner-content position-absolute">
                     <h4>15% off</h4>
                     <h5>Smart Watches</h5>
-                    <p>From $999.00 or $41.62/mo.</p>
+                    <p>From $999.00.</p>
                   </div>
                 </div>
 
-                <div className="small-banner position-relative p-3">
+                <div className="small-banner position-relative">
                   <img
                     className="img-fluid rounded-3"
                     src="images/catbanner-03.jpg"
@@ -59,11 +59,11 @@ const Home = () => {
                   <div className="small-banner-content position-absolute">
                     <h4>new arrivals</h4>
                     <h5>Buy IPads</h5>
-                    <p>From $999.00 or $41.62/mo.</p>
+                    <p>From $999.00.</p>
                   </div>
                 </div>
 
-                <div className="small-banner position-relative p-3">
+                <div className="small-banner position-relative">
                   <img
                     className="img-fluid rounded-3"
                     src="images/catbanner-04.jpg"
@@ -73,6 +73,51 @@ const Home = () => {
                     <h4>free engraving</h4>
                     <h5>HeadPhones</h5>
                     <p>From $999.00 </p>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </section>
+      <section className="home-wrapper-2 py-5">
+        <div className="container-xxl">
+          <div className="row">
+            <div className="col-12">
+              <div className="services d-flex align-items-center justify-content-between">
+                <div className="d-flex align-items-center gap-10">
+                  <img src="images/service.png" alt="services" />
+                  <div>
+                    <h6>Free Shipping</h6>
+                    <p className="mb-0">From all orders over 500</p>
+                  </div>
+                </div>
+                <div className="d-flex align-items-center gap-15">
+                  <img src="images/service-02.png" alt="services" />
+                  <div>
+                    <h6>Daily Surprise Offers</h6>
+                    <p>Save up to 25% off</p>
+                  </div>
+                </div>
+                <div className="d-flex align-items-center gap-15">
+                  <img src="images/service-03.png" alt="services" />
+                  <div>
+                    <h6>Support 24/7</h6>
+                    <p>Shop with an expert</p>
+                  </div>
+                </div>
+                <div className="d-flex align-items-center gap-15">
+                  <img src="images/service-04.png" alt="services" />
+                  <div>
+                    <h6>Affordable Prices</h6>
+                    <p>Get Factory Direct Price</p>
+                  </div>
+                </div>
+                <div className="d-flex align-items-center gap-15">
+                  <img src="images/service-05.png" alt="services" />
+                  <div>
+                    <h6>Secure Payments</h6>
+                    <p>100% Protected Payments</p>
                   </div>
                 </div>
               </div>
