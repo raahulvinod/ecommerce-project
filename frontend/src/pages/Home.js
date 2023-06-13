3cf24eadc2bc9e713cdeaf017226afa72a4a6bf1
Rahul vinod <120365941+raahulvinod@users.noreply.github.com>
2023-06-13 16:11:09 +0530

    Brand Marquee Added

--- frontend/src/pages/Home.js
+++ frontend/src/pages/Home.js
@@ -192,6 +192,45 @@ const Home = () => {
           </div>
         </div>
       </section>
+      <section className="marque-wrapper py-5">
+        <div className="container-xxl">
+          <div className="row">
+            <div className="col-12">
+              <div className="marquee-inner-wrapper card-wrapper">
+                <marquee>
+                  <div className="d-flex">
+                    <div className="mx-4 w-25">
+                      <img src="images/brand-01.png" alt="brand" />
+                    </div>
+                    <div className="mx-4 w-25">
+                      <img src="images/brand-02.png" alt="brand" />
+                    </div>
+                    <div className="mx-4 w-25">
+                      <img src="images/brand-03.png" alt="brand" />
+                    </div>
+                    <div className="mx-4 w-25">
+                      <img src="images/brand-04.png" alt="brand" />
+                    </div>
+                    <div className="mx-4 w-25">
+                      <img src="images/brand-05.png" alt="brand" />
+                    </div>
+                    <div className="mx-4 w-25">
+                      <img src="images/brand-06.png" alt="brand" />
+                    </div>
+                    <div className="mx-4 w-25">
+                      <img src="images/brand-07.png" alt="brand" />
+                    </div>
+                    <div className="mx-4 w-25">
+                      <img src="images/brand-08.png" alt="brand" />
+                    </div>
+                  </div>
+                </marquee>
+              </div>
+            </div>
+          </div>
+        </div>
+      </section>
+      <section className="blog-wrapper py-5 home-wrapper-2"></section>
     </>
   );
 };
