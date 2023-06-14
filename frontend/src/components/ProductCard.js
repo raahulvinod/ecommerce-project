a9fcfebd8037ec6e592597f5cdffd43dd650597d
Rahul vinod <120365941+raahulvinod@users.noreply.github.com>
2023-06-14 18:24:22 +0530

    Our Store With Multiple Grid Systems Added

--- frontend/src/components/ProductCard.js
+++ frontend/src/components/ProductCard.js
@@ -1,48 +1,105 @@
 import ReactStars from 'react-rating-stars-component';
-import { Link } from 'react-router-dom';
+import { Link, useLocation } from 'react-router-dom';
 
-const ProductCard = () => {
+const ProductCard = (props) => {
+  const { grid } = props;
+  // alert(grid);
+  let location = useLocation();
   return (
-    <div className="col-3">
-      <Link className="product-card position-relative">
-        <div className="wishlist-icon position-absolute">
-          <Link>
-            <img src="/images/wish.svg" alt="wishlist" />
-          </Link>
-        </div>
-        <div className="product-image">
-          <img src="/images/cam1.avif" alt="products" className="img-fluid" />
-          <img src="/images/cam2.avif" alt="products" className="img-fluid" />
-        </div>
-        <div className="product-details">
-          <h6 className="brand">FUJIFILM</h6>
-          <h5 className="product-title">
-            FUJIFILM X-T3 Mirrorless Camera X-T3
-          </h5>
-          <ReactStars
-            value={4}
-            edit={false}
-            count={5}
-            size={24}
-            activeColor="#ffd700"
-          />
-          <p className="price">₹ 999.00</p>
-        </div>
-        <div className="action-bar position-absolute">
-          <div className="d-flex flex-column gap-15">
+    <>
+      <div
+        className={`${location.pathname === '/store' ? `gr-${grid}` : 'col-3'}`}
+      >
+        <Link className="product-card position-relative">
+          <div className="wishlist-icon position-absolute">
             <Link>
-              <img src="images/prodcompare.svg" alt="compare" />
-            </Link>
-            <Link>
-              <img src="images/view.svg" alt="view" />
-            </Link>
-            <Link>
-              <img src="images/add-cart.svg" alt="addcart" />
+              <img src="/images/wish.svg" alt="wishlist" />
             </Link>
           </div>
-        </div>
-      </Link>
-    </div>
+          <div className="product-image">
+            <img src="/images/cam1.avif" alt="products" className="img-fluid" />
+            <img src="/images/cam2.avif" alt="products" className="img-fluid" />
+          </div>
+          <div className="product-details">
+            <h6 className="brand">FUJIFILM</h6>
+            <h5 className="product-title">
+              FUJIFILM X-T3 Mirrorless Camera X-T3
+            </h5>
+            <ReactStars
+              value={4}
+              edit={false}
+              count={5}
+              size={24}
+              activeColor="#ffd700"
+            />
+            <p className={`description ${grid === 12 ? 'd-block' : 'd-none'}`}>
+              1 Camera Body with XF 16-80mm Lens, Rechargeable Battery, USB
+              Cable, AC Power Adapter, Headphone Adapter, Shoulder Strap
+            </p>
+            <p className="price">₹ 999.00</p>
+          </div>
+          <div className="action-bar position-absolute">
+            <div className="d-flex flex-column gap-15">
+              <Link>
+                <img src="images/prodcompare.svg" alt="compare" />
+              </Link>
+              <Link>
+                <img src="images/view.svg" alt="view" />
+              </Link>
+              <Link>
+                <img src="images/add-cart.svg" alt="addcart" />
+              </Link>
+            </div>
+          </div>
+        </Link>
+      </div>
+      <div
+        className={`${location.pathname === '/store' ? `gr-${grid}` : 'col-3'}`}
+      >
+        <Link className="product-card position-relative">
+          <div className="wishlist-icon position-absolute">
+            <Link>
+              <img src="/images/wish.svg" alt="wishlist" />
+            </Link>
+          </div>
+          <div className="product-image">
+            <img src="/images/cam1.avif" alt="products" className="img-fluid" />
+            <img src="/images/cam2.avif" alt="products" className="img-fluid" />
+          </div>
+          <div className="product-details">
+            <h6 className="brand">FUJIFILM</h6>
+            <h5 className="product-title">
+              FUJIFILM X-T3 Mirrorless Camera X-T3
+            </h5>
+            <ReactStars
+              value={4}
+              edit={false}
+              count={5}
+              size={24}
+              activeColor="#ffd700"
+            />
+            <p className={`description ${grid === 12 ? 'd-block' : 'd-none'}`}>
+              1 Camera Body with XF 16-80mm Lens, Rechargeable Battery, USB
+              Cable, AC Power Adapter, Headphone Adapter, Shoulder Strap
+            </p>
+            <p className="price">₹ 999.00</p>
+          </div>
+          <div className="action-bar position-absolute">
+            <div className="d-flex flex-column gap-15">
+              <Link>
+                <img src="images/prodcompare.svg" alt="compare" />
+              </Link>
+              <Link>
+                <img src="images/view.svg" alt="view" />
+              </Link>
+              <Link>
+                <img src="images/add-cart.svg" alt="addcart" />
+              </Link>
+            </div>
+          </div>
+        </Link>
+      </div>
+    </>
   );
 };
 
--- frontend/src/pages/OurStore.js
+++ frontend/src/pages/OurStore.js
@@ -1,9 +1,12 @@
-import React from 'react';
+import React, { useState } from 'react';
 import BreadCrumb from '../components/BreadCrumb';
 import Meta from '../components/Meta';
 import ReactStars from 'react-rating-stars-component';
+import ProductCard from '../components/ProductCard';
 
 const OurStore = () => {
+  const [grid, setGrid] = useState(4);
+
   return (
     <>
       <Meta title="Our Store" />
@@ -180,7 +183,85 @@ const OurStore = () => {
                 </div>
               </div>
             </div>
-            <div className="col-9"></div>
+            <div className="col-9">
+              <div className="filter-sort-grid mb-4">
+                <div className="d-flex justify-content-between align-items-center">
+                  <div className="d-flex align-items-center gap-10">
+                    <p className="mb-0 d-block" style={{ width: '100px' }}>
+                      Sort By:
+                    </p>
+                    <select name="" id="" className="form-control form-select">
+                      <option value="manual">Featured</option>
+                      <option value="best-selling" selected="selected">
+                        Best selling
+                      </option>
+                      <option value="title-ascending">
+                        Alphabetically, A-Z
+                      </option>
+                      <option value="title-descending">
+                        Alphabetically, Z-A
+                      </option>
+                      <option value="price-ascending">
+                        Price, low to high
+                      </option>
+                      <option value="price-descending">
+                        Price, high to low
+                      </option>
+                      <option value="created-ascending">
+                        Date, old to new
+                      </option>
+                      <option value="created-descending">
+                        Date, new to old
+                      </option>
+                    </select>
+                  </div>
+                  <div className="d-flex align-items-center gap-10">
+                    <p className="totalProducts mb-0">21 Products</p>
+                    <div className="d-flex gap-10 align-items-center grid">
+                      <img
+                        onClick={() => {
+                          setGrid(3);
+                        }}
+                        src="images/gr4.svg"
+                        className="d-block img-fluid"
+                        alt="grid"
+                      />
+                      <img
+                        onClick={() => {
+                          setGrid(4);
+                        }}
+                        src="images/gr3.svg"
+                        className="d-block img-fluid"
+                        alt="grid"
+                      />
+                      <img
+                        onClick={() => {
+                          setGrid(6);
+                        }}
+                        src="images/gr2.svg"
+                        className="d-block img-fluid"
+                        alt="grid"
+                      />
+
+                      <img
+                        onClick={() => {
+                          setGrid(12);
+                        }}
+                        src="images/gr.svg"
+                        className="d-block img-fluid"
+                        alt="grid"
+                      />
+                    </div>
+                  </div>
+                </div>
+              </div>
+              <div className="products-list pb-5">
+                <div className="d-flex gap-10 flex-wrap">
+                  <ProductCard grid={grid} />
+                  <ProductCard grid={grid} />
+                </div>
+              </div>
+            </div>
           </div>
         </div>
       </div>
