11265df06d14f1fe29dcf67567c6226d4a0919a3
Rahul vinod <120365941+raahulvinod@users.noreply.github.com>
2023-06-22 01:48:00 +0530

    Admin-app Products Listing Backend Db

--- admin-app/src/pages/Productlist.js
+++ admin-app/src/pages/Productlist.js
@@ -1,36 +1,76 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import { Table } from 'antd';
-// import { BiSolidEdit } from 'react-icons/bi';
-// import { AiFillDelete } from 'react-icons/ai';
+import { useDispatch, useSelector } from 'react-redux';
+import { getProducts } from '../features/product/productSlice';
+import { Link } from 'react-router-dom';
+import { FaRegEdit } from 'react-icons/fa';
+import { AiFillDelete } from 'react-icons/ai';
+
+const columns = [
+  {
+    title: 'SNo',
+    dataIndex: 'key',
+  },
+  {
+    title: 'Brand',
+    dataIndex: 'brand',
+    sorter: (a, b) => a.brand.length - b.brand.length,
+  },
+  {
+    title: 'Category',
+    dataIndex: 'category',
+    sorter: (a, b) => a.category.length - b.category.length,
+  },
+  {
+    title: 'Title',
+    dataIndex: 'title',
+    sorter: (a, b) => a.title.length - b.title.length,
+  },
+  {
+    title: 'Color',
+    dataIndex: 'color',
+  },
+  {
+    title: 'Price',
+    dataIndex: 'price',
+    sorter: (a, b) => a.title.length - b.title.length,
+  },
+  {
+    title: 'Actions',
+    dataIndex: 'actions',
+  },
+];
 
 const Productlist = () => {
-  const columns = [
-    {
-      title: 'SNo',
-      dataIndex: 'key',
-    },
-    {
-      title: 'Name',
-      dataIndex: 'name',
-    },
-    {
-      title: 'Product',
-      dataIndex: 'product',
-    },
-    {
-      title: 'Status',
-      dataIndex: 'status',
-    },
-  ];
+  const dispatch = useDispatch();
+  useEffect(() => {
+    dispatch(getProducts());
+  }, []);
+
+  const productState = useSelector((state) => state.product.products);
+
   const data1 = [];
-  for (let i = 0; i < 46; i++) {
+  for (let i = 0; i < productState.length; i++) {
     data1.push({
-      key: i,
-      name: `Edward King ${i}`,
-      product: 'Apple Iphone 13 Pro',
-      status: 'Pending',
+      key: i + 1,
+      brand: productState[i].brand,
+      category: productState[i].category,
+      title: productState[i].title,
+      color: productState[i].color,
+      price: `₹ ${productState[i].price}`,
+      actions: (
+        <>
+          <Link to="/admin">
+            <FaRegEdit className="fs-3 text-danger" />
+          </Link>
+          <Link className="ms-3 fs-3 text-danger" to="/admin">
+            <AiFillDelete />
+          </Link>
+        </>
+      ),
     });
   }
+
   return (
     <div>
       <h3 className="mb-3 title">Products</h3>
