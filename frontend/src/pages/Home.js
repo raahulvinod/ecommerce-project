85f68b2781d61c395d1f44574645f942e859b76b
Rahul vinod <120365941+raahulvinod@users.noreply.github.com>
2023-07-03 16:43:56 +0530

    Featured Collection Making  Dynamic

--- frontend/src/pages/Home.js
+++ frontend/src/pages/Home.js
@@ -211,10 +211,76 @@ const Home = () => {
           <div className="col-12">
             <h3 className="section-heading">Featured Collection</h3>
           </div>
-          <ProductCard />
-          <ProductCard />
-          <ProductCard />
-          <ProductCard />
+
+          {productState &&
+            productState?.map((item, index) => {
+              if (item.tags === 'featured') {
+                return (
+                  <div key={index} className={'col-3'}>
+                    <Link
+                      // to={`${
+                      //   location.pathname === '/'
+                      //     ? '/product/:id'
+                      //     : location.pathname === '/product/:id'
+                      //     ? '/product/:id'
+                      //     : ':id'
+                      // }`}
+                      className="product-card position-relative"
+                    >
+                      <div className="wishlist-icon position-absolute">
+                        <button
+                          className="border-0 bg-transparent"
+                          onClick={() => addToWish(item?._id)}
+                        >
+                          <img src={wish} alt="wishlist" />
+                        </button>
+                      </div>
+
+                      <div className="product-image ">
+                        <img
+                          src={item?.images[0].url}
+                          alt="products"
+                          className="img-fluid"
+                          width={250}
+                        />
+                        <img
+                          src={cam2}
+                          alt="products"
+                          className="img-fluid"
+                          width={250}
+                        />
+                      </div>
+                      <div className="product-details">
+                        <h6 className="brand">{item?.brand}</h6>
+                        <h5 className="product-title">{item?.title}</h5>
+                        <ReactStars
+                          value={+item?.totalrating}
+                          edit={false}
+                          count={5}
+                          size={24}
+                          activeColor="#ffd700"
+                        />
+
+                        <p className="price">₹{item.price}</p>
+                      </div>
+                      <div className="action-bar position-absolute">
+                        <div className="d-flex flex-column gap-15">
+                          <button className="border-0 bg-transparent">
+                            <img src={prodcompare} alt="compare" />
+                          </button>
+                          <button className="border-0 bg-transparent">
+                            <img src={view} alt="view" />
+                          </button>
+                          <button className="border-0 bg-transparent">
+                            <img src={addcart} alt="addcart" />
+                          </button>
+                        </div>
+                      </div>
+                    </Link>
+                  </div>
+                );
+              }
+            })}
         </div>
       </Container>
 
