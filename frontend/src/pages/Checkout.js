6367a495f870089b9efefa7bf92154060f4cf78d
Rahul vinod <120365941+raahulvinod@users.noreply.github.com>
2023-07-06 13:31:11 +0530

    checkout Products Listed

--- frontend/src/pages/Checkout.js
+++ frontend/src/pages/Checkout.js
@@ -1,12 +1,28 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import Meta from '../components/Meta';
 import BreadCrumb from '../components/BreadCrumb';
 import { Link } from 'react-router-dom';
 import { BiArrowBack } from 'react-icons/bi';
 import cam1 from '../images/cam1.avif';
 import Container from '../components/Container';
+import { useDispatch, useSelector } from 'react-redux';
 
 const Checkout = () => {
+  const dispatch = useDispatch();
+  const cartState = useSelector((state) => state?.auth?.cartProducts);
+  const [totalAmount, setTotalAmount] = useState(0);
+
+  console.log(cartState);
+
+  useEffect(() => {
+    let sum = 0;
+    for (let index = 0; index < cartState?.length; index++) {
+      sum =
+        sum +
+        Number(cartState[index].quantity) * Number(cartState[index].price);
+      setTotalAmount(sum);
+    }
+  }, [cartState]);
   return (
     <>
       <Meta title="Checkout" />
@@ -128,44 +144,65 @@ const Checkout = () => {
               </form>
             </div>
           </div>
+
           <div className="col-5">
             <div className="border-bottom py-4">
-              <div className="d-flex gap-10 mb-2 align-align-items-center">
-                <div className="w-75 d-flex gap-10">
-                  <div className="w-25 position-relative">
-                    <span
-                      style={{ top: '-10px', right: '2px' }}
-                      className="badge bg-secondary text-white rounded-circle p-2 position-absolute"
+              {cartState &&
+                cartState?.map((item, index) => {
+                  return (
+                    <div
+                      key={index}
+                      className="d-flex gap-10 mb-2 align-align-items-center"
                     >
-                      1
-                    </span>
-                    <img className="img-fluid" src={cam1} alt="product" />
-                  </div>
-                  <div>
-                    <h5 className="total-price">Fuji Film Xt-4</h5>
-                    <p className="total-price">
-                      Mirrorless Camera Body with XF 16-80mm Lens (Black)
-                    </p>
-                  </div>
-                </div>
-                <div className="flex-grow-1">
-                  <h5 className="total">₹ 20000</h5>
-                </div>
-              </div>
+                      <div className="w-75 d-flex gap-10">
+                        <div className="w-25 position-relative">
+                          <span
+                            style={{ top: '-10px', right: '2px' }}
+                            className="badge bg-secondary text-white rounded-circle p-2 position-absolute"
+                          >
+                            {item?.quantity}
+                          </span>
+                          <img
+                            className=""
+                            src={item?.productId?.images[0]?.url}
+                            alt="product"
+                            width={100}
+                            height={100}
+                          />
+                        </div>
+                        <div>
+                          <h5 className="total-price">
+                            {item?.productId.title}
+                          </h5>
+                          <p className="total-price">{item?.productId.brand}</p>
+                        </div>
+                      </div>
+                      <div className="flex-grow-1">
+                        <h5 className="total">
+                          ₹ {item?.quantity * item?.price}
+                        </h5>
+                      </div>
+                    </div>
+                  );
+                })}
             </div>
             <div className="border-bottom py-4">
               <div className="d-flex justify-content-between align-items-center">
-                <p className="total">Subtotal</p>
-                <p className="total-price">₹ 20000</p>
+                <p className="total">
+                  Price ({cartState?.length ? cartState?.length : ''} Item)
+                </p>
+                <p className="total-price">₹ {totalAmount}</p>
               </div>
               <div className="d-flex justify-content-between align-items-center">
-                <p className="mb-0 total">Shipping</p>
-                <p className="mb-0 total-price">₹ 20000</p>
+                <p className="mb-0 total">Delivery Charges</p>
+                <p className="mb-0 total-price" style={{ color: '#388e3c' }}>
+                  Free
+                </p>
               </div>
             </div>
             <div className="d-flex justify-content-between align-items-center border-bootom py-4">
-              <h4 className="total">Total</h4>
-              <h5 className="total">₹ 20000</h5>
+              <h4 className="total">Total Amount</h4>
+              <h5 className="total">₹ {totalAmount ? totalAmount : 0}</h5>
             </div>
           </div>
         </div>
