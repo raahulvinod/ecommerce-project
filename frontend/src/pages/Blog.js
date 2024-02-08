3a18c15326d8d17c50cc49113e8e6c636212cfd8
Rahul vinod <120365941+raahulvinod@users.noreply.github.com>
2024-02-08 14:25:06 +0530

    feat(blog-card): make blog page responsive

--- frontend/src/pages/Blog.js
+++ frontend/src/pages/Blog.js
@@ -26,7 +26,7 @@ const Blog = () => {
       <BreadCrumb title="Blogs" />
       <Container class1="blog-wrapper home-wrapper-2 py-5">
         <div className="row">
-          <div className="col-3">
+          <div className="col-lg-3">
             <div className="filter-card mb-3">
               <h3 className="filter-title">Shop By Categories</h3>
               <div>
@@ -39,11 +39,11 @@ const Blog = () => {
               </div>
             </div>
           </div>
-          <div className="col-9">
+          <div className="col-lg-9">
             <div className="row">
               {blogState?.map((blog, index) => {
                 return (
-                  <div className="col-6 mb-3" key={index}>
+                  <div className="col-lg-4 col-md-6 col-sm-12 mb-3" key={index}>
                     <BlogCard
                       id={blog?._id}
                       title={blog?.title}
