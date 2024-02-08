e252ef89ff15509d2a63252a4c5a0c62e50958a4
Rahul vinod <120365941+raahulvinod@users.noreply.github.com>
2024-02-08 12:21:03 +0530

    feat(ui): make footer responsive

--- frontend/src/components/Footer.js
+++ frontend/src/components/Footer.js
@@ -1,128 +1,158 @@
 import React from 'react';
-import { Link } from 'react-router-dom/';
 import { BsLinkedin, BsYoutube, BsInstagram, BsFacebook } from 'react-icons/bs';
-import newsletter from '../images/newsletter.png';
 
 const Footer = () => {
   return (
     <>
-      <footer className="py-4">
-        <div className="container-xxl">
-          <div className="row align-items-center">
-            <div className="col-5">
-              <div className="footer-top-data d-flex gap-30 align-items-center">
-                <img src={newsletter} alt="newsletter" />
-                <h2 className="mb-0 text-white">Sign Up for Newsletter</h2>
+      <footer className="text-center text-lg-start text-white">
+        <div className="container-xxl py-4">
+          <section className="">
+            <div className="row">
+              <div className="col-md-3 col-lg-3 col-xl-3 mx-auto mt-3">
+                <h6 className="text-uppercase mb-4 font-weight-bold">
+                  Trendfy
+                </h6>
+                <p>
+                  <a href="/contact" className="text-white">
+                    Contact us
+                  </a>
+                </p>
+                <p>
+                  <a href="/contact" className="text-white">
+                    About us
+                  </a>
+                </p>
+                <p>
+                  <a href="/" className="text-white">
+                    Careers
+                  </a>
+                </p>
+                <p>
+                  <a href="/" className="text-white">
+                    Wholesales
+                  </a>
+                </p>
+              </div>
+
+              <hr className="w-100 clearfix d-md-none" />
+
+              <div className="col-md-2 col-lg-2 col-xl-2 mx-auto mt-3">
+                <h6 className="text-uppercase mb-4 font-weight-bold">
+                  Information
+                </h6>
+                <p>
+                  <a href="/" className="text-white">
+                    Privacy Policy
+                  </a>
+                </p>
+                <p>
+                  <a href="/" className="text-white">
+                    Refund Policy
+                  </a>
+                </p>
+                <p>
+                  <a href="/" className="text-white">
+                    Shipping Policy
+                  </a>
+                </p>
+                <p>
+                  <a href="/" className="text-white">
+                    Terms Of Service
+                  </a>
+                </p>
+              </div>
+
+              <hr className="w-100 clearfix d-md-none" />
+
+              <div className="col-md-3 col-lg-2 col-xl-2 mx-auto mt-3">
+                <h6 className="text-uppercase mb-4 font-weight-bold">
+                  Account
+                </h6>
+                <p>
+                  <a href="/" className="text-white">
+                    Your Account
+                  </a>
+                </p>
+                <p>
+                  <a href="/" className="text-white">
+                    Become an Affiliate
+                  </a>
+                </p>
+                <p>
+                  <a href="/" className="text-white">
+                    Shipping Rates
+                  </a>
+                </p>
+                <p>
+                  <a href="/" className="text-white">
+                    Help
+                  </a>
+                </p>
+              </div>
+
+              <hr className="w-100 clearfix d-md-none" />
+
+              <div className="col-md-4 col-lg-3 col-xl-3 mx-auto mt-3">
+                <h6 className="text-uppercase mb-4 font-weight-bold">
+                  Contact
+                </h6>
+                <p>
+                  <i className="fas fa-home mr-3"></i>No. 63, 1st Floor, 14th
+                  Cross, 9th Main Road, 2nd Stage, Indiranagar, Bengaluru,
+                  Karnataka 560038, India
+                </p>
+                <p>
+                  <i className="fas fa-envelope mr-3"></i>shopping@trendfy.com
+                </p>
+                <p>
+                  <i className="fas fa-phone mr-3"></i> +91 9400000072
+                </p>
               </div>
             </div>
-            <div className="col-7">
-              <div className="input-group ">
-                <input
-                  type="text"
-                  className="form-control py-1"
-                  placeholder="Your Email Address"
-                  aria-label="Your Email Address"
-                  aria-describedby="basic-addon2"
-                />
-                <span className="input-group-text p-2" id="basic-addon2">
-                  Subscribe
-                </span>
-              </div>
-            </div>
-          </div>
-        </div>
-      </footer>
-      <footer className="py-4">
-        <div className="container-xxl">
-          <div className="row">
-            <div className="col-4">
-              <h4 className="text-white mb-4">Contact Us</h4>
-              <div>
-                <address className="text-white fs-6">
-                  Trendfy Internet Private Limited,
-                  <br /> Kollam, 691589,
-                  <br /> Kerala, India.
-                </address>
-                <a
-                  href="tel:+9400000072"
-                  className="mt-3 text-white mb-1 d-block mb-3"
-                >
-                  +91 9400000072
-                </a>
-                <a
-                  href="mailto:trendfy@gmail.com"
-                  className="mt-2 text-white mb-0 d-block mb-3"
-                >
-                  shopping@trendfy.com
-                </a>
-                <div className="social-icons d-flex aligin-items-center gap-30 mt-4">
-                  <a className="text-white" href="https://www.linkedin.com">
-                    <BsLinkedin className="fs-4" />
-                  </a>
-                  <a className="text-white" href="https://www.youtube.com">
-                    <BsYoutube className="fs-4" />
-                  </a>
-                  <a className="text-white" href="https://www.instagram.com">
-                    <BsInstagram className="fs-4" />
-                  </a>
-                  <a className="text-white" href="https://www.facebook.com">
-                    <BsFacebook className="fs-4" />
+          </section>
+
+          <section className="p-3 pt-0">
+            <div className="row d-flex align-items-center">
+              <div className="col-md-7 col-lg-8 text-center text-md-start">
+                <div className="p-3">
+                  &copy; {new Date().getFullYear()}{' '}
+                  <a className="text-white" href="https://trendfy.com/">
+                    Trendfy.com
                   </a>
                 </div>
               </div>
-            </div>
-            <div className="col-3">
-              <h4 className="text-white mb-4">Information</h4>
-              <div className="footer-links d-flex flex-column">
-                <Link to="/privacy-policy" className="text-white py-2 mb-1">
-                  Privacy Policy
-                </Link>
-                <Link to="/refund-policy" className="text-white py-2 mb-1">
-                  Refund Policy
-                </Link>
-                <Link to="/shipping-policy" className="text-white py-2 mb-1">
-                  Shipping Policy
-                </Link>
-                <Link to="/terms-conditions" className="text-white py-2 mb-1">
-                  Terms Of Service
-                </Link>
-                <Link to="/blogs" className="text-white py-2 mb-1">
-                  Blogs
-                </Link>
+
+              <div className="col-md-5 col-lg-4 ml-lg-0 text-center text-md-end">
+                <a
+                  className="text-white btn btn-floating m-1"
+                  href="https://www.linkedin.com"
+                >
+                  <BsLinkedin className="fs-4" />
+                </a>
+
+                <a
+                  className="btn btn-floating m-1 text-white"
+                  href="https://www.youtube.com"
+                >
+                  <BsYoutube className="fs-4" />
+                </a>
+
+                <a
+                  className="btn btn-floating m-1 text-white"
+                  href="https://www.instagram.com"
+                >
+                  <BsInstagram className="fs-4" />
+                </a>
+
+                <a
+                  className="btn btn-floating m-1 text-white"
+                  href="https://www.facebook.com"
+                >
+                  <BsFacebook className="fs-4" />
+                </a>
               </div>
             </div>
-            <div className="col-3">
-              <h4 className="text-white mb-4">Account</h4>
-              <div className="footer-links d-flex flex-column">
-                <Link className="text-white py-2 mb-1">Search</Link>
-                <Link className="text-white py-2 mb-1">About</Link>
-                <Link className="text-white py-2 mb-1">Faq</Link>
-                <Link className="text-white py-2 mb-1">Contact</Link>
-                <Link className="text-white py-2 mb-1">Size chart</Link>
-              </div>
-            </div>
-            <div className="col-2">
-              <h4 className="text-white mb-4">Quick Links</h4>
-              <div className="footer-links d-flex flex-column">
-                <Link className="text-white py-2 mb-1">Accessories</Link>
-                <Link className="text-white py-2 mb-1">Laptops</Link>
-                <Link className="text-white py-2 mb-1">Headphones</Link>
-                <Link className="text-white py-2 mb-1">Smart Watches</Link>
-                <Link className="text-white py-2 mb-1">Tablets</Link>
-              </div>
-            </div>
-          </div>
-        </div>
-      </footer>
-      <footer className="py-4">
-        <div className="container-xxl">
-          <div className="row">
-            <div className="col-12">
-              <p className="text-center text-white mb-0">
-                &copy; {new Date().getFullYear()} Trendfy.com
-              </p>
-            </div>
-          </div>
+          </section>
         </div>
       </footer>
     </>
