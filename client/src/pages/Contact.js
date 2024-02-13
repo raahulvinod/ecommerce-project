a2e1f2829df5b50044905be200b184f821f6906f
Rahul vinod <120365941+raahulvinod@users.noreply.github.com>
2024-02-13 11:33:06 +0530

    feat(contact): contact page make responsive

--- client/src/pages/Contact.js
+++ client/src/pages/Contact.js
@@ -1,6 +1,4 @@
 import React from 'react';
-import Meta from '../components/Meta';
-import BreadCrumb from '../components/BreadCrumb';
 import { AiOutlineHome } from 'react-icons/ai';
 import { BiPhoneCall } from 'react-icons/bi';
 import { AiOutlineMail } from 'react-icons/ai';
@@ -9,6 +7,9 @@ import Container from '../components/Container';
 import * as yup from 'yup';
 import { useFormik } from 'formik';
 import { useDispatch } from 'react-redux';
+
+import Meta from '../components/Meta';
+import BreadCrumb from '../components/BreadCrumb';
 import { createQuery } from '../features/contact/contactSlice';
 
 const contactSchema = yup.object({
@@ -43,7 +44,7 @@ const Contact = () => {
       <BreadCrumb title="Contacts" />
       <Container class1="contact-wrapper py-5 home-wrapper-2">
         <div className="row">
-          <div className="col-12">
+          <div className="col-12 mb-4">
             <iframe
               title="location"
               src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d15768.596115698827!2d76.66124452888792!3d8.865715109025965!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x3b05e365cccaa373%3A0x57142b5967a1c503!2sKottiyam%2C%20Kerala!5e0!3m2!1sen!2sin!4v1686765156458!5m2!1sen!2sin"
@@ -55,9 +56,9 @@ const Contact = () => {
               referrerPolicy="no-referrer-when-downgrade"
             ></iframe>
           </div>
-          <div className="col-12 mt-5">
-            <div className="contact-inner-wrapper d-flex justify-content-between">
-              <div>
+          <div className="row">
+            <div className="col-lg-6 mb-4">
+              <div className="contact-inner-wrapper">
                 <h3 className="contact-title mb-4">Contact</h3>
                 <form
                   onSubmit={formik.handleSubmit}
@@ -128,33 +129,33 @@ const Contact = () => {
                   </div>
                 </form>
               </div>
-              <div>
+            </div>
+            <div className="col-lg-6 mb-4">
+              <div className="contact-inner-wrapper">
                 <h3 className="contact-title mb-4">Get in touch with us</h3>
-                <div>
-                  <ul className="ps-0">
-                    <li className="mb-3 d-flex gap-15 align-items-center">
-                      <AiOutlineHome className="fs-5 " />
-                      <address>
-                        Trendfy Internet Private Limited, Kollam, 691589,
-                        Kerala, India.
-                      </address>
-                    </li>
-                    <li className="mb-3 d-flex gap-15 align-items-center">
-                      <BiPhoneCall className="fs-5" />
-                      <a href="tel:+91 9400000072">+91 9400000072</a>
-                    </li>
-                    <li className="mb-3 d-flex gap-15 align-items-center">
-                      <AiOutlineMail className="fs-5" />
-                      <a href="mailto:shopping@trendfy.com">
-                        shopping@trendfy.com
-                      </a>
-                    </li>
-                    <li className="mb-3 d-flex gap-15 align-items-center">
-                      <BiInfoCircle className="fs-5" />
-                      <p className="mb-0">Monday – Friday 10 AM – 8 PM</p>
-                    </li>
-                  </ul>
-                </div>
+                <ul className="ps-0">
+                  <li className="mb-3 d-flex gap-15 align-items-center">
+                    <AiOutlineHome className="fs-5" />
+                    <address>
+                      No. 63, 1st Floor, 14th Cross, 9th Main Road, 2nd Stage,
+                      Indiranagar, Bengaluru, Karnataka 560038, India
+                    </address>
+                  </li>
+                  <li className="mb-3 d-flex gap-15 align-items-center">
+                    <BiPhoneCall className="fs-5" />
+                    <a href="tel:+91 9400000072">+91 9400000072</a>
+                  </li>
+                  <li className="mb-3 d-flex gap-15 align-items-center">
+                    <AiOutlineMail className="fs-5" />
+                    <a href="mailto:shopping@trendfy.com">
+                      shopping@trendfy.com
+                    </a>
+                  </li>
+                  <li className="mb-3 d-flex gap-15 align-items-center">
+                    <BiInfoCircle className="fs-5" />
+                    <p className="mb-0">Monday – Friday 10 AM – 8 PM</p>
+                  </li>
+                </ul>
               </div>
             </div>
           </div>
