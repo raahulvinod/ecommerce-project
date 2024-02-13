9c1ce81c7fe325860024877957c42c7c6f2c243b
Rahul vinod <120365941+raahulvinod@users.noreply.github.com>
2024-02-13 11:55:07 +0530

    feat(order): make order page responsive

--- client/src/pages/Orders.js
+++ client/src/pages/Orders.js
@@ -21,84 +21,75 @@ const Orders = () => {
         <div className="row">
           <div className="col-12 mt-3">
             {orderState &&
-              orderState?.map((item, index) => {
-                return (
-                  <div
-                    className="row my-3"
-                    style={{ backgroundColor: '#febd69' }}
-                    key={index}
-                  >
-                    <div className="col-12">
-                      <div className="row bg-white p-3">
-                        <div className="col-3">
-                          <h6>Product Details</h6>
-                        </div>
-                        <div className="col-2">
-                          <h6>Quantity</h6>
-                        </div>
-                        <div className="col-2">
-                          <h6>Price</h6>
-                        </div>
-                        <div className="col-2">
-                          <h6>Color</h6>
-                        </div>
-                        <div className="col-3">
-                          <h6>Order Details</h6>
-                        </div>
-                        {item?.orderItems?.map((i, index) => {
-                          return (
-                            <div className="col-12" key={index}>
-                              <div className="row bg-white p-3">
-                                <div className="col-3">
-                                  <p>{i.product?.title}</p>
-                                  <div>
-                                    <img
-                                      className="mb-5"
-                                      style={{ width: 100, height: 100 }}
-                                      src={i.product?.images[0]?.url}
-                                    ></img>
-                                  </div>
-                                </div>
-                                <div className="col-2">
-                                  <p>{i.quantity}</p>
-                                </div>
-                                <div className="col-2">
-                                  <p>₹ {i.price}</p>
-                                </div>
-                                <div className="col-2">
-                                  <ul className="colors">
-                                    <li
-                                      style={{
-                                        backgroundColor: i.color?.title,
-                                      }}
-                                    ></li>
-                                  </ul>
-                                </div>
-                                <div className="col-3">
-                                  <p>
-                                    Order Id: <b>{item?._id}</b>
-                                  </p>
-                                  <p>Total Amount: ₹{item?.totalPrice}</p>
-                                  <p>
-                                    Total After Discount :
-                                    <strong>
-                                      ₹ {item?.totalPriceAfterDiscount}
-                                    </strong>
-                                  </p>
-                                  <p style={{ color: '#26a541' }}>
-                                    &#x2714;
-                                    <strong>{item?.orderStatus}</strong>
-                                  </p>
-                                </div>
+              orderState.map((item, index) => (
+                <div
+                  className="row my-3"
+                  style={{ backgroundColor: '#febd69' }}
+                  key={index}
+                >
+                  <div className="col-12">
+                    <div className="row bg-white p-3">
+                      {item?.orderItems?.map((i, index) => (
+                        <div key={index} className="col-lg-12 mb-3">
+                          <div className="row">
+                            <div className="col-lg-3 col-md-6">
+                              <h6>Product Details</h6>
+                              <p>{i.product?.title}</p>
+                              <div>
+                                <img
+                                  className="mb-3"
+                                  style={{
+                                    width: '100%',
+                                    maxWidth: '100px',
+                                    height: 'auto',
+                                  }}
+                                  src={i.product?.images[0]?.url}
+                                  alt="Product"
+                                />
                               </div>
                             </div>
-                          );
-                        })}
-                      </div>
+                            <div className="col-lg-2 col-md-6">
+                              <h6>Quantity</h6>
+                              <p>{i.quantity}</p>
+                            </div>
+                            <div className="col-lg-2 col-md-6">
+                              <h6>Price</h6>
+                              <p>₹ {i.price}</p>
+                            </div>
+                            <div className="col-lg-2 col-md-6">
+                              <h6>Color</h6>
+                              <ul className="colors">
+                                <li
+                                  style={{
+                                    backgroundColor: i.color?.title,
+                                  }}
+                                ></li>
+                              </ul>
+                            </div>
+                            <div className="col-lg-3 col-md-6">
+                              <h6>Order Details</h6>
+                              <p>
+                                Order Id: <b>{item?._id}</b>
+                              </p>
+                              <p>Total Amount: ₹{item?.totalPrice}</p>
+                              <p>
+                                Total After Discount :{' '}
+                                <strong>
+                                  ₹ {item?.totalPriceAfterDiscount}
+                                </strong>
+                              </p>
+                              <p style={{ color: '#26a541' }}>
+                                &#x2714;
+                                <strong>{item?.orderStatus}</strong>
+                              </p>
+                            </div>
+                          </div>
+                        </div>
+                      ))}
                     </div>
                   </div>
-                );
-              })}
+                </div>
+              ))}
           </div>
         </div>
       </Container>
