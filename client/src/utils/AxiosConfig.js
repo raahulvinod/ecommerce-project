13120a6c13498dd57c29a020e9b75cccfdebf061
Rahul vinod <120365941+raahulvinod@users.noreply.github.com>
2024-02-15 16:18:04 +0530

    refactor(config): update base url config

--- client/src/utils/AxiosConfig.js
+++ client/src/utils/AxiosConfig.js
@@ -1,4 +1,4 @@
-export const base_url = process.env.REACT_APP_BASE_URL;
+export const base_url = 'http://localhost:5000/api/';
 
 const getTokenFromLocalStorage = localStorage.getItem('customer')
   ? JSON.parse(localStorage.getItem('customer'))
