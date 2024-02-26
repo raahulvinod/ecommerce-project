d0dafe3d5ed9dc374207fa4f79e22175b620ced5
Rahul vinod <120365941+raahulvinod@users.noreply.github.com>
2024-02-26 15:56:28 +0530

    fix(server): fix genreate token JWT typo issue

--- server/config/jwtToken.js
+++ server/config/jwtToken.js
@@ -1,5 +1,5 @@
 import jwt from 'jsonwebtoken';
 
 export const generateToken = (id) => {
-  return jwt.sign({ id }, process.env.JWt_SECRET, { expiresIn: '1d' });
+  return jwt.sign({ id }, process.env.JWT_SECRET, { expiresIn: '1d' });
 };
