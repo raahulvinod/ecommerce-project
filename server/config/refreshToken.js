4cb1575e89c5f8b916aa46000b2074b1666285d2
Rahul vinod <120365941+raahulvinod@users.noreply.github.com>
2024-02-26 15:41:53 +0530

    fix(server): fix generate refreshtoken issue

--- server/config/refreshToken.js
+++ server/config/refreshToken.js
@@ -1,4 +1,7 @@
 import jwt from 'jsonwebtoken';
+import dotenv from 'dotenv';
+
+dotenv.config();
 
 export const generateRefreshToken = (id) => {
   return jwt.sign({ id }, process.env.JWt_SECRET, { expiresIn: '3d' });
