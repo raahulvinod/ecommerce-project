af12d67ff36b48eb6a3814f9cb0072776cb0990a
Rahul vinod <120365941+raahulvinod@users.noreply.github.com>
2024-02-27 11:03:02 +0530

    fix(server): update image upload directory paths for image multer storage

--- server/middlewares/uploadImages.js
+++ server/middlewares/uploadImages.js
@@ -11,6 +11,7 @@ const __dirname = dirname(__filename);
 const uploadDirectory =
   process.env.UPLOAD_DIRECTORY || path.join(__dirname, '../public/images/');
 
+
 export const multerStorage = multer.diskStorage({
   destination: function (req, file, cb) {
     cb(null, uploadDirectory);
@@ -25,7 +26,7 @@ export const multerFilter = (req, file, cb) => {
   if (file.mimetype.startsWith('image')) {
     cb(null, true);
   } else {
-    cb(new Error('Unsupported file format'), false); // Use Error constructor for consistent error handling
+    cb({ message: 'Unsupported file format' }, false);
   }
 };
 
@@ -36,43 +37,31 @@ export const uploadPhoto = multer({
 });
 
 export const productImgResize = async (req, res, next) => {
-  try {
-    if (!req.file) return next();
-
-    await Promise.all(
-      req.files.map(async (file) => {
-        await sharp(file.path)
-          .resize(300, 300)
-          .toFormat('jpeg')
-          .jpeg({ quality: 90 })
-          .toFile(path.join(uploadDirectory, 'products', file.filename)); // Use path.join to ensure correct directory structure
-        fs.unlinkSync(file.path);
-      })
-    );
-
-    next();
-  } catch (err) {
-    next(err); // Forward the error to the error handler
-  }
+  if (!req.file) return next();
+  await Promise.all(
+    req.files.map(async (file) => {
+      await sharp(file.path)
+        .resize(300, 300)
+        .toFormat('jpeg')
+        .jpeg({ quality: 90 })
+        .toFile(`public/images/products/${file.filename}`);
+      fs.unlinkSync(`public/images/products/${file.filename}`);
+    })
+  );
+  next();
 };
 
 export const blogImgResize = async (req, res, next) => {
-  try {
-    if (!req.file) return next();
-
-    await Promise.all(
-      req.files.map(async (file) => {
-        await sharp(file.path)
-          .resize(300, 300)
-          .toFormat('jpeg')
-          .jpeg({ quality: 90 })
-          .toFile(path.join(uploadDirectory, 'blogs', file.filename)); // Use path.join to ensure correct directory structure
-        fs.unlinkSync(file.path);
-      })
-    );
-
-    next();
-  } catch (err) {
-    next(err); // Forward the error to the error handler
-  }
+  if (!req.file) return next();
+  await Promise.all(
+    req.files.map(async (file) => {
+      await sharp(file.path)
+        .resize(300, 300)
+        .toFormat('jpeg')
+        .jpeg({ quality: 90 })
+        .toFile(`public/images/blogs/${file.filename}`);
+      fs.unlinkSync(`public/images/blogs/${file.filename}`);
+    })
+  );
+  next();
 };
